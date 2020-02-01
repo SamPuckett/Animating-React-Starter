168c91cdedaa630191ce85d6409539054db36c93
Samuel Ruiz <samuelpuckett77@gmail.com>
2020-01-31 21:33:34 -0800

    Lecture #7 - Example 1: Animated Nav
    - we created a spring that translates the a component from the right and slides into place.
    - we created a [isNavOpen, setNavOpen] state variable to toggle the translate3d of the nav.
    - we created the nav
    - we added a className to the nav that was provided by Scott.
    - We imported { animted } from 'react-spring'; insdie of our nav element.
    - we imported the Nav component into our App.js
    - we placed the <Nav /> component right underneath our line of code that created our button.
    - We tied an onClick property to the button that was already on the page to toggle the 'isNavOpen' value w/ 'setNavOpen'.
    - we passed the navAnimation spring we created into our Nav as a style={navAnimation}
    - we converted our div inside of the Nav.jsx component from a normal <div> to an <animated.div>
    - we destructured the {style} that was passed in as a prop inside of our Nav component from our App component.
    - We set the style of the <animated.div> element to the prop that we just destructured. style={style}

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSpring, animated } from "react-spring";
 import Toggle from "./Toggle";
+import Nav from "./Nav";
 import logo from "./logo.svg";
 import "./App.css";
 
 const App = () => {
+  const [isNavOpen, setNavOpen] = useState(false);
+  const navAnimation = useSpring({
+    transform: isNavOpen
+      ? `translate3d(0, 0, 0) scale(1)`
+      : `translate3d(100%, 0, 0) scale(0.6)`
+  });
   const fade = useSpring({
     from: {
       opacity: 0
@@ -12,14 +19,14 @@ const App = () => {
     opacity: 1
   });
 
-  // Shorter
-  // const fade = useSpring({ from: { opacity: 0 }, opacity: 1 });
-
   return (
     <animated.div className='App' style={fade}>
       <header className='App-header'>
         <img src={logo} className='logo' alt='logo' />
-        <button className='menu-button'>Menu</button>
+        <button onClick={() => setNavOpen(!isNavOpen)} className='menu-button'>
+          Menu
+        </button>
+        <Nav style={navAnimation} />
       </header>
       <main>
         <Toggle />
---
+++ src/Nav.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { animated } from "react-spring";
+
+const Nav = ({ style }) => {
+  return (
+    <animated.div className='nav-wrapper' style={style}>
+      <a href='#'>Home</a>
+      <a href='#'>About</a>
+      <a href='#'>Store</a>
+      <a href='#'>Tutorials</a>
+    </animated.div>
+  );
+};
+
+export default Nav;
