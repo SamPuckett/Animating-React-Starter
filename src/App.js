c02b943b347824aa906661237678ac2e8ec68ed9
Samuel Ruiz <samuelpuckett77@gmail.com>
2020-01-31 18:26:35 -0800

    Lecture # 6
    - add interpolation to make an animation that does transform3d and changes the middle value uisng a spring

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import { useSpring, animated } from 'react-spring';
-import Toggle from './Toggle';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useSpring, animated } from "react-spring";
+import Toggle from "./Toggle";
+import logo from "./logo.svg";
+import "./App.css";
 
 const App = () => {
   const fade = useSpring({
@@ -16,16 +16,16 @@ const App = () => {
   // const fade = useSpring({ from: { opacity: 0 }, opacity: 1 });
 
   return (
-    <animated.div className="App" style={fade}>
-      <header className="App-header">
-        <img src={logo} className="logo" alt='logo' />
-        <button className="menu-button">Menu</button>
+    <animated.div className='App' style={fade}>
+      <header className='App-header'>
+        <img src={logo} className='logo' alt='logo' />
+        <button className='menu-button'>Menu</button>
       </header>
       <main>
         <Toggle />
       </main>
     </animated.div>
   );
-}
+};
 
 export default App;
